7d8502d0767777dbdc40fe56b988430f42adb782
Kevin_was_here <xuyang4@ualberta.ca>
2022-07-08 10:58:29 -0600

    pain

---
+++ Easy/twosum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        first = 0
+        second = 1
+
+        #Good memory use but very slow solution
+        while((nums[first]+nums[second] != target) or (first == second)):
+            getAns = target - nums[first]
+            try:
+                second = nums.index(getAns)
+                if second == first:
+                    first += 1
+            except ValueError:
+                first+=1
+
+        if first > second:
+            temp = first
+            first = second
+            second = temp
+        
+        return [first,second]
