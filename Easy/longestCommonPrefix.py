7a7bc9d73c22585409d3d64871e6f3a8c87a1971
Kevin_was_here <xuyang4@ualberta.ca>
2022-07-08 11:40:01 -0600

    Another terrible solution

--- Easy/longestCommonPrefix.py
+++ Easy/longestCommonPrefix.py
@@ -1,14 +1,23 @@
-str = ["ccc","acc","c"]
-
-out = ""
-i = 0
-cur = (str[0])[i]
-
-while True:
-    if all(cur in string[:i+1] for string in str):
-        out = cur
-        i += 1
-        cur += (str[0])[i]
-    else: 
-        print(out)
-        break
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        out = ""
+        i = 0
+        try:
+            cur = strs[0][i]
+        except:
+            return ""
+        while True:
+            if all(cur in string[:i+1] for string in strs):
+                out = cur
+                i += 1
+                try:
+                    cur += (strs[0])[i]
+                except:
+                    return out
+            else: 
+                return(out)
+                break
